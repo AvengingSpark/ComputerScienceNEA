3beb84b44d7ef5054be9c825cd5390701b544535
Ciaran Soden <ciaran.soden@hotmail.co.uk>
2022-02-01 08:30:04 +0000

    Added printGrid and printBoxes

--- Automated Solver/sudokuGridClass.py
+++ Automated Solver/sudokuGridClass.py
@@ -3,31 +3,31 @@ from FieldClass import Field
 class SudokuGrid:
     """
     Every instance of this class respresents a Sudoku Grid. There should only be one instance of this class at a time. An object of this class has five attributes:
-    Private List Grid - The sudoku grid. This will be a 2D list, where each nested list represents a row within the grid, and each element within the nested array points to an instance of the Field class (a cell)
-    Private Integer numOfClues - This represents the number of clues that are in the sudoku grid. For a sudoku grid to be solvable, there must be at least 17 clues in the grid. This enables there to be a check whenever a grid is initialised that there are at least 17 clues on the grid.
-    Private Dictionary clueLocations - This will represent where every clue is located on the grid. It will be formatted as '"[row][cell]": value'. This will be used when generating the grid to defined what cells can be edited or cannot be edited.
-    Private Boolean isSolved - This boolean shows whether the puzzle has been solved or not. Depending on whether all of the cells in the grid have been filled.
-    Private Array Boxes - This 3D array represents the 9 3x3 boxes that make up a sudoku grid. Each inital nested array refers to one of the 9 boxes, where the other array is a row in the box and each element in a row points to an instance of the class Field
+    Private List Grid - The sudoku __grid. This will be a 2D list, where each nested list represents a row within the __grid, and each element within the nested array points to an instance of the Field class (a cell)
+    Private Integer numOfClues - This represents the number of clues that are in the sudoku __grid. For a sudoku __grid to be solvable, there must be at least 17 clues in the __grid. This enables there to be a check whenever a __grid is initialised that there are at least 17 clues on the __grid.
+    Private Dictionary clueLocations - This will represent where every clue is located on the __grid. It will be formatted as '"[row][cell]": value'. This will be used when generating the __grid to defined what cells can be edited or cannot be edited.
+    Private Boolean isSolved - This boolean shows whether the puzzle has been solved or not. Depending on whether all of the cells in the __grid have been filled.
+    Private Array Boxes - This 3D array represents the 9 3x3 boxes that make up a sudoku __grid. Each inital nested array refers to one of the 9 boxes, where the other array is a row in the box and each element in a row points to an instance of the class Field
     """
 
     def __init__(self, clueLocations):
-        #The clueLocations parameter is a dictionary which refers to the different clues in the sudoku grid. 
+        #The clueLocations parameter is a dictionary which refers to the different clues in the sudoku __grid. 
         self.__grid = []
         self.__numOfClues = len(clueLocations)
         self.__clueLocations = clueLocations
         self.__isSolved = False
-        #Calls generateGrid method to populate the grid with instances of the class Field.
+        #Calls generateGrid method to populate the __grid with instances of the class Field.
         self.generateGrid()
-        #
-        self.__boxes = [self.__grid[:3][:3], self.__grid[3:6][:3], self.__grid[6:9][:3], self.__grid[:3][3:6], self.__grid[3:6][3:6], self.__grid[6:9][3:6], self.__grid[:3][6:9], self.__grid[3:6][6:9], self.__grid[6:9][6:9]]
+        #This represents all of the boxes within the sudoku grid.
+        self.__boxes = [[self.__grid[0][:3], self.__grid[1][:3], self.__grid[2][:3]], [self.__grid[0][3:6], self.__grid[1][3:6], self.__grid[2][3:6]], [self.__grid[0][6:], self.__grid[1][6:], self.__grid[2][6:]], [self.__grid[3][:3], self.__grid[4][:3], self.__grid[5][:3]], [self.__grid[3][3:6], self.__grid[4][3:6], self.__grid[5][3:6]], [self.__grid[3][6:], self.__grid[4][6:], self.__grid[5][6:]], [self.__grid[6][:3], self.__grid[7][:3], self.__grid[8][:3]], [self.__grid[6][3:6], self.__grid[7][3:6], self.__grid[8][3:6]], [self.__grid[6][6:], self.__grid[7][6:], self.__grid[8][6:]]]
         
     def generateGrid(self):
-    #This methods populates the sudoku grid with pointer to instances of the Field class.
+    #This methods populates the sudoku __grid with pointer to instances of the Field class.
         for row in range(9):
-            #Loops through 9 times corresponding the 9 row in a sudoku grid
+            #Loops through 9 times corresponding the 9 row in a sudoku __grid
             newRow = []
             for cell in range(9):
-                #Loops through 9 times corresponding the 9 cells in a row of the sudoku grid.
+                #Loops through 9 times corresponding the 9 cells in a row of the sudoku __grid.
                 if f"[{row}][{cell}]" in self.__clueLocations:
                     #Checks whether the cell is in the dictionary of clue locations using the same string format - if it is, the instance of the field class for that cell will be considered a clue and have a non-0 value.
                     newRow.append(Field(self.__clueLocations[f"[{row}][{cell}]"]))
@@ -46,12 +46,23 @@ class SudokuGrid:
                 tempString += (f"{cell.getValue()}    ")
             print(tempString, "\n")
     
+    def printBoxes(self):
+        boxNum = 1
+        for box in self.__boxes:
+            print(f"Box {boxNum}")
+            boxNum += 1
+            for row in box:
+                output = ""
+                for cell in row:
+                    output += (f"{cell.getValue()}    ")
+                print(output, "\n")
+    
     def resetGrid(self):
         for row in range(len(self.__grid)):
             for cell in range(len(self.__grid[row])):
                 #Iterates through each row and each cell within each row. If the current cell is not a clue, the possibilities of this cell are reset to the digits 1-9
                 if not self.__grid[row][cell].isClue():
-                    self.grid[row][cell].resetPossibilities()
+                    self.__grid[row][cell].resetPossibilities()
     
     def checkIfSolved(self):
     #This function returns the value of the Boolean isSolved
@@ -59,5 +70,5 @@ class SudokuGrid:
     
     
 test1 = SudokuGrid({})
-test1.printGrid()
+test1.printBoxes()
 input()
